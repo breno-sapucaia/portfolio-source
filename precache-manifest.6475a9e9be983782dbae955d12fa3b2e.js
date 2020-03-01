6f9b2a1e9dbea09dcb09074a04e795aba14487ca
Breno <brenoss.magalhaes@gmail.com>
2020-02-29 23:00:24 -0300

    setando arquivos estaticos na master

---
+++ precache-manifest.6475a9e9be983782dbae955d12fa3b2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532daab524385fc365e91c7f3ab0e3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6bc31ab9bf24457d0ed",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b928feefffe1ffdea554",
+    "url": "/static/css/main.68aae59f.chunk.css"
+  },
+  {
+    "revision": "e6bc31ab9bf24457d0ed",
+    "url": "/static/js/2.a210c438.chunk.js"
+  },
+  {
+    "revision": "ab7c16e7a77bf6f969db06d484ca8708",
+    "url": "/static/js/2.a210c438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b928feefffe1ffdea554",
+    "url": "/static/js/main.703b8e29.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "cd140a61b846ced24bf40dd10a4ffbae",
+    "url": "/static/media/nebulosa.cd140a61.jpg"
+  }
+]);
